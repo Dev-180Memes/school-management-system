3425994b7b41299ea75ea908d2b97600fd7d26ac
Adeoluwa Agbakosi <adeoluwaagbakosi@gmail.com>
2024-03-19 13:21:24 +0100

    login api for staff and student login

---
+++ src/pages/api/staff/login.ts
@@ -0,0 +1,67 @@
+import { Teacher } from "@/models";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { NextApiRequest, NextApiResponse } from "next";
+import db from "@/utils/connectDb";
+import { Interface } from "readline";
+
+const JWT_SECRET: string = "FUNAAB_HIGH_SCHOL";
+
+interface RequestData {
+    email: string,
+    password: string
+}
+
+interface ResponseData {
+    message: string,
+    token?: string
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+    await db;
+
+    if (req.method === "POST") {
+        const { email, password } = req.body as RequestData;
+
+        try {
+            const teacher = await Teacher.findOne({ email });
+
+            if (!teacher) {
+                return res.status(404).json({
+                    message: "Teacher with this email does not exist"
+                })
+            }
+
+            const isPasswordValid = await bcrypt.compare(password, teacher.password);
+
+            if (!isPasswordValid) {
+                return res.status(400).json({
+                    message: "Incorrect Password"
+                })
+            }
+
+            const token = jwt.sign({
+                id: teacher._id,
+                name: teacher.name,
+                email: teacher.email,
+                className: teacher.className,
+                account: "staff"
+            }, JWT_SECRET, {
+                expiresIn: "24h"
+            });
+
+            return res.status(200).json({
+                message: "Teacher logged in successfully",
+                token
+            })
+        } catch (error) {
+            return res.status(500).json({
+                message: "Failed to login"
+            })
+        }
+    } else {
+        return res.status(405).json({
+            message: "Method Not Allowed"
+        })
+    }
+}
---
+++ src/pages/api/student/login.ts
@@ -0,0 +1,61 @@
+import { Student } from "@/models";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { NextApiRequest, NextApiResponse } from "next";
+import db from "@/utils/connectDb";
+
+const JWT_SECRET: string = "FUNAAB_HIGH_SCHOOL";
+
+interface RequestData {
+    email: string,
+    password: string
+}
+
+interface ResponseData {
+    message: string,
+    token?: string
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+    await db;
+
+    if (req.method === "POST") {
+        const { email, password } = req.body as RequestData;
+
+        try {
+            const student = await Student.findOne({ email });
+
+            if (!student) {
+                return res.status(404).json({
+                    message: "Student with this email does not exist"
+                })
+            }
+
+            const isPasswordValid = await bcrypt.compare(password, student.password);
+
+            if (!isPasswordValid) {
+                return res.status(400).json({
+                    message: "Incorrect Password"
+                })
+            }
+
+            const token = jwt.sign({
+                id: student._id,
+                name: student.name,
+                email: student.email,
+                account: "student"
+            }, JWT_SECRET, {
+                expiresIn: "24h"
+            });
+
+            return res.status(200).json({
+                message: "Student logged in successfully",
+                token
+            })
+        } catch (error) {
+            return res.status(500).json({
+                message: "Failed to login"
+            })
+        }
+    }
+}
